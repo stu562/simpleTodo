c67b41afcb04f91c26b6b06ec77c2662c3aca795
Steven Tu <stu562@gmail.com>
2019-02-16 21:26:47 -0800

    fix bug of infinite loop

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
 var todo = ['walk the dog'];
-var userAction = prompt('what would you like to do?');
+var userAction = prompt(`what would you like to do? \ntype "help" for more info`);
 
 while (userAction !== "quit") {
   if (userAction === "list") {
@@ -8,8 +8,10 @@ while (userAction !== "quit") {
     var newAdd = prompt('what to add to list?')
     todo.push(newAdd);
     console.log(todo);
-  } 
+  } else if (userAction === "help"){
+    alert(`"new" for todo.\n"list" to show all todo.\n"quit" to quit application`)
+  }
   //ask again
-  var userAction = prompt('what would you like to do?');
+var userAction = prompt(`what would you like to do? \ntype "help" for more info`);
 }
 console.log('you have quit application')
