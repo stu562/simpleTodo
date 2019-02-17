bfcb9569182d2c37e1914db159ee22269da0c34e
Steven Tu <stu562@gmail.com>
2019-02-16 21:10:31 -0800

    created todo list application, working

--- todo.js
+++ todo.js
@@ -1,1 +1,15 @@
-alert('testing')
+var todo = ['walk the dog'];
+var userAction = prompt('what would you like to do?');
+
+while (userAction !== "quit") {
+  if (userAction === "list") {
+    console.log(todo);
+  } else if (userAction === "new") {
+    var newAdd = prompt('what to add to list?')
+    todo.push(newAdd);
+    console.log(todo);
+  } 
+  //ask again
+  var userAction = prompt('what would you like to do?');
+}
+console.log('you have quit application')
